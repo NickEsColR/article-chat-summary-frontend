316b0ae83382ba696796e951d96763af6cf8a7af
NickEsColR <nicoesol0818@gmail.com>
2024-05-09 19:42:22 -0500

    feat 2: add summary modal

---
+++ src/article/components/SummaryModal.jsx
@@ -0,0 +1,33 @@
+import Modal from "react-modal";
+
+const customStyles = {
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+    },
+};
+
+export const SummaryModal = () => {
+    return (
+        <Modal
+            isOpen={true}
+            style={customStyles}
+            className={"modal"}
+            overlayClassName={"bg-modal"}
+            closeTimeoutMS={200}
+        >
+            <h1>Summary</h1>
+            <hr />
+            <p>
+                Nisi nulla nostrud duis exercitation qui enim cillum enim
+                exercitation amet. Culpa minim Lorem nisi esse. Velit sit ut
+                amet id adipisicing duis aute mollit laboris. Enim laboris
+                incididunt id nulla ipsum minim aliqua elit sunt.
+            </p>
+        </Modal>
+    );
+};
--- src/article/pages/ArticlePage.jsx
+++ src/article/pages/ArticlePage.jsx
@@ -1,16 +1,20 @@
 import { Sidebar } from "../";
 import { ChatView, NothingSelectedView } from "../../chat";
+import { SummaryModal } from "../components/SummaryModal";
 
 export const ArticlePage = () => {
     return (
-        <div className="row p-0">
-            <div className="col-3 p-0">
-                <Sidebar />
+        <>
+            <div className="row p-0">
+                <div className="col-3 p-0">
+                    <Sidebar />
+                </div>
+                <div className="col-9 p-0">
+                    {/* <NothingSelectedView /> */}
+                    <ChatView />
+                </div>
             </div>
-            <div className="col-9 p-0">
-                {/* <NothingSelectedView /> */}
-                <ChatView />
-            </div>
-        </div>
+            <SummaryModal />
+        </>
     );
 };
